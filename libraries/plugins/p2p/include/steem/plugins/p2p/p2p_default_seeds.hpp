3e9338dd1e035c8d1e5421ce83e18349d34950c9
takeitez <nhpquyit@gmail.com>
2019-03-12 11:31:23 +0700

    initiation for testnet

--- libraries/plugins/p2p/include/steem/plugins/p2p/p2p_default_seeds.hpp
+++ libraries/plugins/p2p/include/steem/plugins/p2p/p2p_default_seeds.hpp
@@ -7,11 +7,7 @@ namespace steem{ namespace plugins { namespace p2p {
 #ifdef IS_TEST_NET
 const std::vector< std::string > default_seeds;
 #else
-const std::vector< std::string > default_seeds = {
-   "seed-east.steemit.com:2001",          // steemit
-   "seed-central.steemit.com:2001",       // steemit
-   "seed-west.steemit.com:2001"          // steemit
-};
+const std::vector< std::string > default_seeds;
 #endif
 
 } } } // steem::plugins::p2p
